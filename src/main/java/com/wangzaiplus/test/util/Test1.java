554b92baac987d16efa468a7bb323a70ea41381e
luobin <kingluobin@live.cn>
2024-01-28 20:14:53 +0800

    qqq

---
+++ src/main/java/com/wangzaiplus/test/util/Test1.java
@@ -0,0 +1,11 @@
+package com.wangzaiplus.test.util;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.regex.Pattern;
+
+public class Test1 {
+    public static void isMobile(String mobile) {
+        return ;
+    }
+}
---
+++ src/main/java/com/wangzaiplus/test/util/Test2.java
@@ -0,0 +1,7 @@
+package com.wangzaiplus.test.util;
+
+public class Test2 {
+    public static void isMobile(String mobile) {
+        Test1.isMobile(mobile);
+    }
+}
